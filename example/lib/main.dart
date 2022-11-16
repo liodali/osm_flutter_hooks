e9a0ce5d7800d809c8b214365eec4eb7c3eab835
liodali <lionelviperdali@gmail.com>
2022-11-16 19:39:51 +0100

    add example to osm_flutter_hooks

---
+++ example/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:osm_flutter_hooks/osm_flutter_hooks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: SimpleOSM(),
+      ),
+    );
+  }
+}
+
+class SimpleOSM extends HookWidget {
+  const SimpleOSM({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = useMapController(initMapWithUserPosition: true);
+    useMapIsReady(
+      controller: controller,
+      mapIsReady: () async {
+        await controller.setZoom(zoomLevel: 15);
+      },
+    );
+    return OSMFlutter(
+      controller: controller,
+      markerOption: MarkerOption(
+        defaultMarker: const MarkerIcon(
+          icon: Icon(
+            Icons.person_pin_circle,
+            color: Colors.blue,
+            size: 56,
+          ),
+        ),
+      ),
+      trackMyPosition: false,
+    );
+  }
+}
